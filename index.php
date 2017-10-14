b14ed1d6a7eab1a289ca5071a6bfda8772b65f95
Daniel Ferolino <drferolino@gmail.com>
2017-10-14 03:58:57 -0600

    d

--- index.php
+++ index.php
@@ -17,12 +17,11 @@
     </head>
 
     <body>
-        <?php
-            foreach($users as $user) :
-        ?>
+        <?php foreach($users as $user) :?>
         <div class="jumbotron">
             <h2><?php echo $user['name'];?></h2>
         </div>
+        <?php endforeach; ?>
     </body>
 
 </html>
