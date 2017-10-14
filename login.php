8d6fbe72198edfc35385a46d536c32eebd75f7c8
Daniel Ferolino <drferolino@gmail.com>
2017-10-14 06:28:05 -0600

    f

--- login.php
+++ login.php
@@ -4,13 +4,12 @@
         // Get Form Data
         $name = mysqli_real_escape_string($conn, $_POST['name']);
         $password = mysqli_real_escape_string($conn, $_POST['password']);
-        echo "<script type='text/javascript'>alert('beforesql')</script>";
         $query = "SELECT COUNT(*) FROM Users WHERE (name='$name' AND password='$password')";
         $result = mysqli_query($conn, $query);
         if(mysqli_query($conn, $query)) {
             echo 'Success';
-            var_dump($result);
             if ($result['field_count'] == 1) {
+                echo "<script type='text/javascript'>alert('logging in')</script>";
                 header('Location: home.php/');
             }
         } else {
