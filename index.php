967b2f589cf9a58b9f2559336a1b5247ec92cffa
Daniel Ferolino <drferolino@gmail.com>
2017-10-14 03:45:21 -0600

    asdf

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-    include ('config.php');
     // Create Connection
     $conn = mysqli_connect('localhost', 'root', 'givv-master', 'Test');
 
@@ -14,8 +13,8 @@
 
     $result = mysqli_query($conn, $query);
     echo "<script type='text/javascript'>alert('after sql');</script>";
-    //$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    var_dump($result);
+    $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    var_dump($users);
 
     //echo "<script type='text/javascript'>console.log('test');</script>";
     
@@ -31,7 +30,6 @@
             <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
             <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
             <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-            <link href='./style.css' rel='stylesheet' type='text/css'>
             <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
 
